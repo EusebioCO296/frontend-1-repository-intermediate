4156ed0eec98c04433abb18846fb65a62b18c63f
EusebioCO296 <eusebiocaballero7@gmail.com>
2026-05-27 00:20:22 -0500

    Reto To Do List finalizado

--- TODOLIST/main.js
+++ TODOLIST/main.js
@@ -1,37 +1,66 @@
 const userInput = document.getElementById("userInput");
-const checkBox = document.querySelector('input[type="checkbox"]');
-let contador = 1; //Contador para asignar un id único a cada nuevo div creado.
+
+let contador = 0; //Contador para asignar un id único a cada nuevo div creado.
+let completas = 0; //Contador para llevar el seguimiento de las tareas completadas.
+let id = 0; //Contador para asignar un id único a cada nuevo div creado.
 
 userInput.addEventListener('submit', (event) => {
 
   event.preventDefault(); //Previene el comportamiento predeterminado del formulario. 
-  
+  id++; //Incrementa el contador de id cada vez que se envía el formulario.
   contador++; //Incrementa el contador cada vez que se envía el formulario.
-  
   const nuevoDiv = document.createElement('div')
+  nuevoDiv.classList.add('class-container')
+  nuevoDiv.id = `${id}` //Agrega un id al nuevo div creado.
+  nuevoDiv.innerHTML = `
+    <input type="checkbox">
+    <label>${userInput.elements[0].value}</label>
+    <img src="./images/eliminar.png" alt="Delete" class="closeBtn">
+    `; //Agrega el valor del input al nuevo div creado.
+
   const contenedor = document.querySelector('.list-container')
   contenedor.appendChild(nuevoDiv)
-  nuevoDiv.classList.add('class-container')
-  nuevoDiv.id = `${contador}` //Agrega un id al nuevo div creado.
-  nuevoDiv.innerHTML = `
-  <input type="checkbox">
-  <label>${userInput.elements[0].value}</label>
-  <img src="./images/eliminar.png" alt="Delete" class="closeBtn">
-  `; //Agrega el valor del input al nuevo div creado.
+
+  userInput.elements[0].value = '';
+  
   stats.innerHTML = `
-    <div>Tareas Pendientes: ${contador} Tareas Completadas: 1</div>`
-});
+    <div>Tareas Pendientes: ${contador} Tareas Completadas: ${completas}</div>
+    `
 
+  const label = nuevoDiv.querySelector('label'); //Selecciona el label dentro del nuevo div creado.
 
-checkBox.addEventListener('change', (event) => {
-  const checkbox = event.target; //Obtiene el checkbox que se ha cambiado.
-  const label = checkbox.nextElementSibling; //Obtiene el label asociado al checkbox.
-  if (checkbox.checked) {
-    label.style.textDecoration = 'line-through'; //Si el checkbox está marcado, agrega una línea a través del texto del label.
-    const stats = document.getElementById('stats');
+  nuevoDiv.querySelector('input[type="checkbox"]').addEventListener('change', (event) => {
+    
+    if (event.target.checked) {
+      completas++; //Incrementa el contador de tareas completadas cada vez que se marca un checkbox.
+      contador--; 
+      label.style.textDecoration = 'line-through'; //Si el checkbox está marcado, agrega una línea a través del texto del label.
+      const stats = document.getElementById('stats');
+      
+    } else {
+      completas--; //Decrementa el contador de tareas completadas cada vez que se desmarca un checkbox.
+      contador++;
+      label.style.textDecoration = 'none'; //Si el checkbox no está marcado, elimina la línea a través del texto del label.
+    }  
     stats.innerHTML = `
-    <div>Tareas Pendientes:1  Tareas Completadas: 1</div>`
-  } else {
-    label.style.textDecoration = 'none'; //Si el checkbox no está marcado, elimina la línea a través del texto del label.
-  }
+        <div>Tareas Pendientes: ${contador}  Tareas Completadas: ${completas}</div>
+        `;
+  });
+
+  nuevoDiv.querySelector('.closeBtn').addEventListener('click', () => {
+
+    if (nuevoDiv.querySelector('input[type="checkbox"]').checked) {
+      completas--; // Si la tarea estaba marcada como completada, actualiza el conteo de completadas
+    } else {
+    id--;
+    contador--; // Decrementa el contador de tareas pendientes al eliminar una tarea
+    }
+    nuevoDiv.remove(); // Elimina el div del DOM
+    stats.innerHTML = `
+        <div>Tareas Pendientes: ${contador}  Tareas Completadas: ${completas}</div>
+        `;
+
+  });  
+
 });
+
